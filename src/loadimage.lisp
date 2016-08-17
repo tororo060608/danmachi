0f121d71c3e9830c181242577f470dadf79b777e
tororo060608 <hiroto060608@gmail.com>
2016-08-17 16:46:02 +0900

    atk stuck

--- src/loadimage.lisp
+++ src/loadimage.lisp
@@ -15,4 +15,13 @@
 		   :test-bullet "test_bullet.png"
 		   :wall "wall.png"
 		   :floor "floor.png"
+		   :sword-front1 "sword-front1.png"
+		   :sword-front2 "sword-front2.png"
+		   :sword-back1 "sword-back1.png"
+		   :sword-back2 "sword-back2.png"
+		   :sword-right1 "sword-right1.png"
+		   :sword-right2 "sword-right2.png"
+		   :sword-left1 "sword-left1.png"
+		   :sword-left2 "sword-left2.png"
 		   ))
+
--- src/player.lisp
+++ src/player.lisp
@@ -5,6 +5,9 @@
   (player-speed 5)
   (width 64)
   (height 64)
+  (player-state nil)
+  (atk-stuck (make-timer 10))
+  (atk-time-limit (make-timer 10))
   (image (get-image :player_front)))
 
 (defmethod add-object ((p player) (game game))
@@ -18,28 +21,36 @@
 	(call-next-method))))
 
 (defmethod update ((p player) (game game))
-  (with-accessors ((vx vx) (vy vy) (x point-x) (y point-y)
-		   (speed player-speed)) p
-    (with-slots (up down right left z) (keystate game)
-      (cond ((key-pressed-p right) (setf vx speed))
-	    ((key-pressed-p left)  (setf vx (- speed)))
-	    (t (setf vx 0)))
-      (cond ((key-pressed-p up)   (setf vy (- speed)))
-	    ((key-pressed-p down) (setf vy speed))
-	    (t (setf vy 0)))
-      (cond ((key-down-p z) (attack p game))))
-    
-    ;; slanting move
-    (when (and (/= vx 0) (/= vy 0))
-      (setf vx (/ vx (sqrt 2))
-	    vy (/ vy (sqrt 2)))))
+  (cond ((eq (player-state p) 'atk-s) (when (funcall (atk-stuck p))
+					(attack p game)
+					(setf (player-state p) 'atk-now)))
+	((eq (player-state p) 'atk-now) (when (funcall (atk-time-limit p))
+					  (setf (player-state p) 'atk-e)))
+	((eq (player-state p) 'atk-e) (when (funcall (atk-stuck p))
+					(setf (player-state p) nil)))
+	(t (with-accessors ((vx vx) (vy vy) (x point-x) (y point-y)
+			    (speed player-speed)) p
+	     (with-slots (up down right left z) (keystate game)
+	       (cond ((key-pressed-p right) (setf vx speed))
+		     ((key-pressed-p left)  (setf vx (- speed)))
+		     (t (setf vx 0)))
+	       (cond ((key-pressed-p up)   (setf vy (- speed)))
+		     ((key-pressed-p down) (setf vy speed))
+		     (t (setf vy 0)))
+	       (cond ((key-down-p z) (atk-start p game))))
+	
+	     ;; slanting move
+	     (when (and (/= vx 0) (/= vy 0))
+	       (setf vx (/ vx (sqrt 2))
+		     vy (/ vy (sqrt 2)))))))
+	   
   (call-next-method))
-
+  
 (defmethod attack ((p player) (game game))
   (add-object (make-instance 'player-attack) game))
 
 (define-class player-attack (bullet)
-  (width 16)
+  (width 64)
   (height 32)
   (atk 50)
   (time-limit (make-timer 10)))
@@ -56,3 +67,12 @@
 			     :point-x (point-x p)
 			     :point-y (+ (point-y p) (height p)))
 	      game))
+
+
+(defmethod atk-start ((p player) (game game))
+  (setf (player-state p) 'atk-s
+	(vx p) 0
+	(vy p) 0))
+
+(defmethod atk-end ((p player) (game game))
+  (setf (player-state p) 'atk-e))
